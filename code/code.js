60c64e561278895dddce6f4c147a9ad3d8278d02
clau4774 <claudiodangiolo@gmail.com>
2021-02-10 08:24:13 -0300

    Function of slider complete, doing discount function

--- code/code.js
+++ code/code.js
@@ -4,9 +4,24 @@ const price = document.querySelector('.form-component__span');
 const views = document.querySelector('#views');
 const btn = document.querySelector('.form-component__button');
 
-const changeValues = () => {
+const changeValues = (e) => {
     let arrViews = ['10K', '50K', '100K', '500K', '1M'];
     let arrPrice = ['$8.00', '$12.00', '$16.00', '$24.00', '$36.00'];
+    const inputValue = e.target.value;
+    
+    for (let x = 0; x < arrViews.length ; x++) {
+        if(inputValue == x) {
+            return  (price.innerText = arrPrice[x], views.innerText = arrViews[x]);
+        }
+    }
+    
 }
 
-slider.addEventListener('change', changeValues, false);
+const discount = (e) => {
+    const value = e.target.checked;
+    console.log(value)
+}
+
+slider.addEventListener('input', changeValues, false);
+
+check.addEventListener('click', discount, false);
