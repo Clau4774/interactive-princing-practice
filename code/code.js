27e0baf1c0dcaa06ecb14c857c87c5f6fdb567f1
clau4774 <claudiodangiolo@gmail.com>
2021-02-11 07:51:38 -0300

    Almost done, polishing mi JS code

--- code/code.js
+++ code/code.js
@@ -1,46 +1,58 @@
 const slider = document.querySelector('.form-component__input-slider');
-const check = document.querySelector('.form-component__input-checkbox');
+const checkBox = document.querySelector('.form-component__input-checkbox');
 const price = document.querySelector('.form-component__span');
 const views = document.querySelector('#views');
 const btn = document.querySelector('.form-component__button');
 
 let state = false;
 
-const getValue = () => {
-    let val =slider.value;
-    console.log(val)
-    changeValues(val);
-}
-
-
-
-const changeValues = (e) => {
-    let arrViews = ['10K', '50K', '100K', '500K', '1M'];
-    let arrPrice = [8, 12, 16, 24, 36];
-    let discount = arrPrice.map(a => {
+const arrViews = ['10K', '50K', '100K', '500K', '1M'];
+    const arrPrice = [8, 12, 16, 24, 36];
+    const disc = arrPrice.map(a => {
        return (a-(a*.25));
     });
+
+const getValue = () => {
     
+    let val = slider.value;
+    changeValues(val);
+    return val
+}
+
+const changeValues = (e) => {
+
     const inputValue = e;
     
-    !state ? (price.innerText = `$${arrPrice[inputValue]}.00 `, views.innerText = arrViews[inputValue]) :  (price.innerText = `$${discount[inputValue]}.00 `, views.innerText = arrViews[inputValue]);
+    let data = [];
 
-    return !state ? arrPrice[inputValue] : discount[inputValue];
+    !state ? (price.innerText = `$${arrPrice[inputValue]}.00 `, views.innerText = arrViews[inputValue]) :  (price.innerText = `$${disc[inputValue]}.00 `, views.innerText = arrViews[inputValue]);
+
+    !state ? data.push(arrViews[inputValue],arrPrice[inputValue],state) : data.push(arrViews[inputValue],disc[inputValue],state);
+
+
+    return data;
 
 }
 const discount = (e) => {
-    
-    
     state = !state;
-    
     getValue();
-
 }
 
-const confirmPay = (e) => {
+const confirmService = (evt) => {
+
+    evt.preventDefault();
+    let val = changeValues(getValue());
+    let descActive = '';
+    val[2] ? descActive = '25% of discount' : descActive = 'no discount';
+    let conf = confirm(`You has been selected the ${val[0]} pageviews whit ${descActive} for a price of $${val[1]}.00.
     
+    Want to confirm?`)
+    const id = Math.round(Math.random()*10000);
+    console.log(val[0] + ' '+val[1] +' ' + val[2]+ ' '+ id);
 }
 
 slider.addEventListener('input', getValue, false);
 
-check.addEventListener('click', discount, false);
+checkBox.addEventListener('click', discount, false);
+
+btn.addEventListener('click', confirmService, false);
